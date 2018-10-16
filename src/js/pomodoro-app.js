650a7b8f4ba8fac7f03e957a21143fef443fa235
Mariam Domínguez <maryanpopis@hotmail.com>
2018-10-15 18:33:09 -0600

    change of pomodoro file name

---
+++ src/js/pomodoro-app.js
@@ -0,0 +1,57 @@
+const breakTime = 5;
+const workTime = 25;
+let totalTime = workTime * 60;
+let timer;
+let isBreak = true;
+const startBtn = document.getElementById("start-btn");
+startBtn.addEventListener("click", startPomodoro);
+const stopBtn = document.getElementById("stop-btn");
+stopBtn.addEventListener("click", stopTimer);
+
+function startPomodoro() {
+  timer = setInterval(() => {
+    countdown(true);
+  }, 1000);
+}
+
+function countdown() {
+  if (totalTime >= 1) {
+    totalTime--;
+  } else {
+    playAudio();
+    totalTime = 0;
+    totalTime = getTime() * 60;
+    isBreak = !isBreak;
+  }
+  displayTimeLeft();
+}
+
+function playAudio() {
+  let audio = new Audio(
+    "http://www.orangefreesounds.com/wp-content/uploads/2016/12/Short-ringtone-for-notification.mp3"
+  );
+  audio.play();
+}
+
+function getTime() {
+  return isBreak ? breakTime : workTime;
+}
+
+function stopTimer() {
+  clearInterval(timer);
+  timer = null;
+}
+
+function padTime(time) {
+  return `${time < 10 ? "0" : ""}${time}`;
+}
+// prettier-ignore
+function displayTimeLeft() {
+    const minutes = Math.floor(totalTime / 60);
+    const seconds = totalTime - (minutes * 60);
+    const timeMinutes = document.getElementById("time-left");
+    const display = `${padTime(minutes)} : ${padTime(seconds)}`;
+
+    timeMinutes.textContent = display;
+    document.title = display;
+  }
