76659e23838857fc2cb57d1dff9df9e8040bb63c
Mariam Domínguez <maryanpopis@hotmail.com>
2018-10-19 19:20:02 -0600

    fix(modal) fix the exports

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,6 @@
 const modal = document.getElementById("modal-task");
 const btnAddTask = document.getElementById("btn-modal");
 const btnClose = document.getElementsByClassName("close")[0];
-
 btnAddTask.addEventListener("click", openModal);
 btnClose.addEventListener("click", closeModal);
 window.addEventListener("click", clickOutside);
@@ -10,9 +9,9 @@ function openModal() {
   modal.style.display = "block";
 }
 
-export const closeModal = () => {
+export function closeModal() {
   modal.style.display = "none";
-};
+}
 
 function clickOutside(e) {
   if (e.target === modal) {
